1a24d4afbf8eba9236b7af0c2e30b23de64b23f9
frean <fespeche@gmail.com>
2022-09-17 08:03:58 -0300

    updates

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
 import chalk from "chalk"
-import { DateTime } from "luxon"
-import { Page } from "puppeteer"
-import { TUserStatus } from "../types/user"
+import {DateTime} from "luxon"
+import {Page} from "puppeteer"
+import {TUserStatus} from "../types/user"
 
 // environment
 const BASE_URL = "https://lichess.org"
@@ -10,70 +10,70 @@ const USER_STATUS_ENDPOINT = `${BASE_URL}/api/users/status`
 
 // helpers
 export const now = (): string => {
-	const now = DateTime.now().toObject()
-	return `[${now.hour}:${now.minute}:${now.second}]`
+  const now = DateTime.now().toObject()
+  return `[${now.hour}:${now.minute}:${now.second}]`
 }
 
 export const getUserStatus = async (user: string): Promise<TUserStatus> => {
-	const endpoint = `${USER_STATUS_ENDPOINT}?ids=${user}&withGameIds=true`
-	const res = await axios.get(endpoint)
-	return res.data[0]
+  const endpoint = `${USER_STATUS_ENDPOINT}?ids=${user}&withGameIds=true`
+  const res = await axios.get(endpoint)
+  return res.data[0]
 }
 
 export const getActiveGame = async (
-	user: string
+  user: string
 ): Promise<string | undefined> => {
-	const userStatus = await getUserStatus(user)
-	return userStatus?.playingId
+  const userStatus = await getUserStatus(user)
+  return userStatus?.playingId
 }
 
 export const login = async (
-	page: Page
-): Promise<{ status: string; error?: any }> => {
-	const user = process.env.LICHESS_USERNAME
-	const password = process.env.LICHESS_PASSWORD
-	if (!user) {
-		return {
-			status: "fail",
-			error: `No LICHESS_USERNAME env found`,
-		}
-	}
-	if (!password) {
-		return {
-			status: "fail",
-			error: `No LICHESS_PASSWORD env found`,
-		}
-	}
+  user: string,
+  password: string,
+  page: Page
+): Promise<{status: string; error?: any}> => {
+  if (!user) {
+    return {
+      status: "fail",
+      error: 'No user provided',
+    }
+  }
+  if (!password) {
+    return {
+      status: "fail",
+      error: 'No password provided',
+    }
+  }
 
-	console.log(now(), `attempting to log in as: ${chalk.cyan(user)}`)
+  console.log(now(), `logging in as: ${chalk.cyan(user)}`)
 
-	try {
-		// fill in form
-		await page.goto("https://lichess.org/login?referrer=/")
-		await page.type("#form3-username", user!)
-		await page.type("#form3-password", password!)
-		await page.click("button.submit")
+  try {
+    // fill in form
+    await page.goto("https://lichess.org/login?referrer=/")
+    await page.type("#form3-username", user)
+    await page.type("#form3-password", password)
+    await page.click("button.submit")
 
-		await page.waitForNavigation()
-		return { status: "success" }
-	} catch (e) {
-		// fail
-		return { status: "failed", error: e }
-	}
+    await page.waitForNavigation()
+    return {status: "success"}
+  } catch (e) {
+    // fail
+    return {status: "failed", error: e}
+  }
 }
 
 export const spectateGame = async (page: Page, gameId: string) => {
-	try {
-		await page.goto(`https://lichess.org/${gameId}`, {
-			waitUntil: "networkidle2",
-		})
+  try {
+    await page.goto(`https://lichess.org/${gameId}`, {
+      waitUntil: "networkidle2",
+    })
 
-		console.log(now(), chalk.yellow(`spectating game ${gameId} :)`))
-	} catch (e) {
-		console.error(
-			now(),
-			chalk.red("ERROR:") + `couldnt spectate game ${gameId}`
-		)
-	}
-	return
+    console.log(now(), chalk.yellow(`spectating game ${gameId} :)`))
+  } catch (e) {
+    console.error(
+      now(),
+      chalk.red("ERROR:") + `couldnt spectate game ${gameId}`
+    )
+  }
+  return
 }
--- src/index.ts
+++ src/index.ts
@@ -1,70 +1,79 @@
-import { launch } from "puppeteer"
+import {launch} from "puppeteer"
+import minimist from "minimist"
 import "dotenv/config"
 import chalk from "chalk"
-import { getActiveGame, now, spectateGame, login } from "./helpers/index.js"
+import {getActiveGame, now, spectateGame, login} from "./helpers/index.js"
 import fs from "fs"
 
 const log = (message: string) => {}
 
-const run = async (user: string) => {
-	const browser = await launch({ headless: true })
-	const page = await browser.newPage()
+const run = (async () => {
+  const {user, password, victim} = minimist(process.argv.slice(2))
 
-	/**
-	 * Log in to Lichess
-	 */
-	const lichessLogin = await login(page)
-	if (lichessLogin.error) {
-		// fail
-		console.error(now(), `${chalk.red(`ERROR:`)} ${lichessLogin.error}`)
-		return
-	}
-	console.log(now(), chalk.green("success!"))
-	console.log()
+  if (!victim) {
+    throw new Error('No victim provided')
+  }
 
-	/**
-	 * Watch & spectate
-	 */
+  if (!user) {
+    throw new Error('No user provided')
+  }
 
-	// create log stream
-	const stream = fs.createWriteStream(`spectated-games/${user}.txt`, {
-		flags: "a",
-	})
+  if (!password) {
+    throw new Error('No password provided')
+  }
 
-	// track retries and lastGameId
-	let lastGameId: undefined | string
-	let retries = 0
+  const browser = await launch({headless: true})
+  const page = await browser.newPage()
 
-	// start watching
-	console.log(now(), `waiting for ${chalk.blue(user)} to start a game..`)
-	await setInterval(async () => {
-		// get gameId
-		let gameId = await getActiveGame(user)
 
-		if (gameId && gameId !== lastGameId) {
-			// update lastGameId
-			lastGameId = gameId
-			retries = 0
+  /**
+   * Log in to Lichess
+   */
+  const lichessLogin = await login(user, password, page)
+  if (lichessLogin.error) {
+    // fail
+    console.error(now(), `${chalk.red(`ERROR:`)} ${lichessLogin.error}`)
+    return
+  }
+  console.log(now(), chalk.green("success!"))
+  console.log()
 
-			console.log(now(), chalk.yellow(`${chalk.blue(user)} is playing!`))
-			console.log(now(), "attempting to spectate")
+  /**
+   * Watch & spectate
+   */
 
-			await spectateGame(page, gameId)
+  // create log stream
+  const stream = fs.createWriteStream(`spectated-games/${victim}.txt`, {
+    flags: "a",
+  })
 
-			stream.write(`${now()} gameId: ${gameId} \n`)
-		} else if (!gameId) {
-			// only log at runtime and once every 30 minutes
-			retries++
-			if (retries === 1 || !(retries % 60)) {
-				console.log(now(), `still no news :(`)
-			}
-		}
-	}, 6000)
-}
+  // track retries and lastGameId
+  let lastGameId: undefined | string
+  let retries = 0
 
-const user = process.argv.slice(2).shift()
-if (!user) {
-	console.error(now(), `${chalk.red("ERROR:")} no user provided`)
-} else {
-	run(user)
-}
+  // start watching
+  console.log(now(), `waiting for ${chalk.blue(victim)} to start a game..`)
+  await setInterval(async () => {
+    // get gameId
+    let gameId = await getActiveGame(victim)
+
+    if (gameId && gameId !== lastGameId) {
+      // update lastGameId
+      lastGameId = gameId
+      retries = 0
+
+      console.log(now(), chalk.yellow(`${chalk.blue(victim)} is playing!`))
+      console.log(now(), "attempting to spectate")
+
+      await spectateGame(page, gameId)
+
+      stream.write(`${now()} gameId: ${gameId} \n`)
+    } else if (!gameId) {
+      // only log at runtime and once every 30 minutes
+      retries++
+      if (retries === 1 || !(retries % 60)) {
+        console.log(now(), `still no news :(`)
+      }
+    }
+  }, 15000)
+})()
